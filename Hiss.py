cae67b1f6c5922f95f02c34d9b96a4110bdcc3e4
TheHoboDad <axejak056@gmail.com>
2024-11-13 13:51:07 +0100

    commit

--- Hiss.py
+++ Hiss.py
@@ -1,16 +1,20 @@
 import pyinputplus as pyip # pip install pyinputplus
+import time
 import random
 
-print ("test")
 totalFloors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 currentFloor = 0
 pickUpFloor = []
 targetFloors = []
+sleepTimer = 1
 
-
+time.sleep(sleepTimer)
 print(totalFloors)
+time.sleep(sleepTimer) # Pause for 1 second
+print(f"Currently on floor: {currentFloor}")
 
-targetFloors.append(pyip.inputInt(prompt="Which floor do you want to go to? ", min=0, max=10)) # Input which floor you want to go to, only ints between 0-10
+
+targetFloors.append(pyip.inputInt(prompt="Which floor do you want to go to? ", min=1, max=10)) # Input which floor you want to go to, only ints between 1-10
 
 
 if (targetFloors[0] > currentFloor): # Select your direction
@@ -20,6 +24,8 @@ else:
 
 print(f"Elevator is currently on floor: {currentFloor}") 
 print(f"The elevator is going to {targetFloors[0]}")
+time.sleep(sleepTimer) # Pause for 1 second
+
 
 
 while True: # Someone getting on the elevator
@@ -37,27 +43,61 @@ while True: # Someone getting on the elevator
     else: 
         newDirection = 'DOWN'
         
-    print(f"A person on floor {onFloor} wants to go  to floor {goToFloor}. Their direction = {newDirection}, your direction = {direction}")
-    break # To stop it from creating multiple npcs 
+    print(f"A person on floor {onFloor} wants to go to floor {goToFloor}. Their direction = {newDirection}, your direction = {direction}")
+    break
+time.sleep(sleepTimer) # Pause for 1 second
     
 if (onFloor >= currentFloor and newDirection == direction):
     pickUpFloor.append(onFloor)
     targetFloors.append(goToFloor)
     
 while targetFloors or pickUpFloor:
-    print(f"The elevator is currently on floor {currentFloor}")
     
-    if currentFloor in targetFloors:
-        print(f"Elevator reached floor {currentFloor}. Staying to let passengers out.")
-        targetFloors.remove(currentFloor)
-
-    if currentFloor in pickUpFloor:
-        print(f"Elevator reached floor {currentFloor}. Staying to let passengers in.")
-        pickUpFloor.remove(currentFloor)
-
     if direction == 'UP':
         currentFloor += 1
     else:
         currentFloor -= 1
+        
+    if currentFloor in targetFloors:
+        time.sleep(sleepTimer) # Pause for 1 second
+        print(f"Elevator reached floor {currentFloor}. Staying to let passengers ut.")
+        targetFloors.remove(currentFloor)
 
-print("All passengers have reached their destinations.")
+    if currentFloor in pickUpFloor:
+        time.sleep(sleepTimer) # Pause for 1 second
+        print(f"Elevator reached floor {currentFloor}. Staying to let passengers in.")
+        pickUpFloor.remove(currentFloor)
+        
+    else:
+        time.sleep(sleepTimer) # Pause for 1 second
+        print(f"The elevator is currently on floor {currentFloor}")
+
+
+time.sleep(sleepTimer) # Pause for 1 second
+# print("All passengers have reached their destinations.")
+
+# if (onFloor <= currentFloor and newDirection != direction):
+#     direction = 'DOWN'
+#     pickUpFloor.append(onFloor)
+#     targetFloors.append(goToFloor)
+#     while targetFloors or pickUpFloor:
+#         if currentFloor in targetFloors:
+#             time.sleep(sleepTimer) # Pause for 1 second
+#             print(f"Elevator reached floor {currentFloor}. Staying to let passengers out.")
+#             targetFloors.remove(currentFloor)
+
+#         elif currentFloor in pickUpFloor:
+#             time.sleep(sleepTimer) # Pause for 1 second
+#             print(f"Elevator reached floor {currentFloor}. Staying to let passengers in.")
+#             pickUpFloor.remove(currentFloor)
+#         else:
+#             time.sleep(sleepTimer) # Pause for 1 second
+#             print(f"The elevator is currently on floor {currentFloor}")
+
+#         if direction == 'UP':
+#             currentFloor += 1
+#         else:
+#             currentFloor -= 1
+#     time.sleep(sleepTimer) # Pause for 1 second
+#     print("All passengers have reached their destinations.")
+# # Programmet slutar ifall npcn vill neråt istället för upp
